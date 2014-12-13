1f6190a4198153130eadc8dd7f3e4d9ed031c2c5
karabaja4 <karabaja4@gmail.com>
2014-12-13 15:38:30 +0100

    data layer

--- SEETech/Hack.DataLayer/Entities/City.cs
+++ SEETech/Hack.DataLayer/Entities/City.cs
@@ -3,10 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using App.DataLayer;
 
 namespace Hack.DataLayer.Entities
 {
-    public sealed class City
+    public sealed class City : Entity
     {
         public string Name { get; set; }
         public string POBox { get; set; }
---
+++ SEETech/Hack.DataLayer/Repositories/CitiesRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using App.DataLayer.Repositories;
+using Hack.DataLayer.Entities;
+
+namespace Hack.DataLayer.Repositories
+{
+    class CitiesRepository : Repository<City>
+    {
+
+    }
+}
