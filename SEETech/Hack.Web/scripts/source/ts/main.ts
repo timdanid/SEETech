67cc38ee6c167ab0811096c8c32cf879cc0d7efe
AgentEd <agented2013@gmail.com>
2014-12-13 18:23:38 +0100

    Router

--- SEETech/Hack.Web/scripts/source/ts/main.ts
+++ SEETech/Hack.Web/scripts/source/ts/main.ts
@@ -2,7 +2,11 @@
 /// <reference path="../../typing/sammy/sammyjs.d.ts" />
 
 var app: Sammy.Application = Sammy();
-app.get('#:foobar', (context) => {
-    console.log(this.params.foobar);
+var router: Hack.Router = new Hack.Router("http://hackapi.azurewebsites.net/api/");
+
+app.get('#/:abc', (context: Sammy.EventContext) => {
+    console.log(context.params.abc);
+    context.partial("abc", (context: Sammy.RenderContext) => {
+    });
 });
-app.run('#');
+app.run('#/');
