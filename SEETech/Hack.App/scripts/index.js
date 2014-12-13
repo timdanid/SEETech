e7ea2f8e9347b639fb140628ca8e16275b8dd46b
AgentGecko <danica.pivalica@gmail.com>
2014-12-13 20:36:55 +0100

    hack app cord

--- SEETech/Hack.App/scripts/index.js
+++ SEETech/Hack.App/scripts/index.js
@@ -6,12 +6,13 @@
     "use strict";
 
     document.addEventListener( 'deviceready', onDeviceReady.bind( this ), false );
+   
 
     function onDeviceReady() {
         // Handle the Cordova pause and resume events
         document.addEventListener( 'pause', onPause.bind( this ), false );
         document.addEventListener( 'resume', onResume.bind( this ), false );
-        
+        addMap();
         // TODO: Cordova has been loaded. Perform any initialization that requires Cordova here.
     };
 
@@ -22,14 +23,14 @@
     function onResume() {
         // TODO: This application has been reactivated. Restore application state here.
     };
+    function addMap()
+    {
+        var div = document.getElementById("map_canvas");
+        var map = plugin.google.maps.Map.getMap(div);
+        console.log(map);
+        map.on(plugin.google.maps.event.MAP_READY, onMapInit);
+    }
+    function onMapInit(map) {
+    }
 })();
 
-bootcards.init({
-    offCanvasHideOnMainClick: true,
-    enableTabletPortraitMode: true,
-    disableRubberBanding: true
-});
-
-window.addEventListener('load', function () {
-    FastClick.attach(document.body);
-}, false);
