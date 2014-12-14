d574bcda2f077474314a9c800ac9ef493ec44201
AgentGecko <danica.pivalica@gmail.com>
2014-12-14 06:07:24 +0100

    Markeri

--- SEETech/Hack.Web/scripts/source/ts/Markers.ts
+++ SEETech/Hack.Web/scripts/source/ts/Markers.ts
@@ -8,6 +8,7 @@ module Hack {
     export class Marker {
         public name: string;
         public location: string;
+        public id: string;
 
         public x_coordinate: Int32Array;
         public y_coordinate: Int32Array;
@@ -15,13 +16,14 @@ module Hack {
         public title: string;
         public icon: string;
 
-        constructor(Name: string, Location: string, X: Int32Array, Y: Int32Array, Title: string, Icon: string) {
+        constructor(Name: string, Location: string, X: Int32Array, Y: Int32Array, Title: string, Icon: string, Id: string) {
             this.name = Name;
             this.location = Location;
             this.x_coordinate = X;
             this.y_coordinate = Y;
             this.title = Title;
             this.icon = Icon;
+            this.id = Id;
         }
 
     }
