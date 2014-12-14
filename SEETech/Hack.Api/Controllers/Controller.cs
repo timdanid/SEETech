42879cade6d35a3f86f57e477146501ece82db23
karabaja4 <karabaja4@gmail.com>
2014-12-14 02:39:34 +0100

    ajme

--- SEETech/Hack.Api/Controllers/Controller.cs
+++ SEETech/Hack.Api/Controllers/Controller.cs
@@ -4,36 +4,54 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using System.Web.Http.Cors;
+using Hack.DataLayer;
+using Newtonsoft.Json;
 
 namespace Hack.Api.Controllers
 {
+    [EnableCors("*", "*", "*")]
     public class Controller : ApiController
     {
-        // GET api/<controller>
-        public IEnumerable<string> Get()
-        {
-            return new string[] { "value1", "value2" };
-        }
+        //// GET api/<controller>
+        //public IEnumerable<string> Get()
+        //{
+        //    return new string[] { "value1", "value2" };
+        //}
 
-        // GET api/<controller>/5
-        public string Get(int id)
-        {
-            return "value";
-        }
+        //// GET api/<controller>/5
+        //public string Get(int id)
+        //{
+        //    return "value";
+        //}
 
-        // POST api/<controller>
-        public void Post([FromBody]string value)
-        {
-        }
+        //// POST api/<controller>
+        //public void Post([FromBody]string value)
+        //{
+        //}
 
-        // PUT api/<controller>/5
-        public void Put(int id, [FromBody]string value)
-        {
-        }
+        //// PUT api/<controller>/5
+        //public void Put(int id, [FromBody]string value)
+        //{
+        //}
 
-        // DELETE api/<controller>/5
-        public void Delete(int id)
+        //// DELETE api/<controller>/5
+        //public void Delete(int id)
+        //{
+        //}
+
+        public string GetCitiesForCounty(string countyName)
         {
+            using (HackEntities entities = new HackEntities())
+            {
+                County county = entities.Counties.FirstOrDefault(x => x.Name.Contains(countyName));
+                if (county != null)
+                {
+                    return JsonConvert.SerializeObject(county.Cities);
+                }
+
+                return null;
+            }
         }
     }
 }
